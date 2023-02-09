a44a0ed30d3a3c77085b763eda2c6f0f22c10ca5
sanscrid6 <65036584+sanscrid6@users.noreply.github.com>
2023-02-09 21:55:29 +0300

    up: 4 part

---
+++ scripts/lab1/4.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION generate_insert(ex_id NUMBER) RETURN VARCHAR2 IS
+    ex_val NUMBER;
+    answer VARCHAR2(30);
+
+CURSOR ct1 IS
+    SELECT val
+    FROM MyTable
+    WHERE id = ex_id;
+
+BEGIN
+    OPEN ct1;
+    FETCH ct1 INTO ex_val;
+    CLOSE ct1;
+
+    RETURN utl_lms.format_message('INSERT INTO MyTable(id, val) VALUES(%s, %s)', TO_CHAR(ex_id), TO_CHAR(ex_val));
+END;
